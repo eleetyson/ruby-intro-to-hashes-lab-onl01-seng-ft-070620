c0dcd553228d7f2f5bad2bf016e282c4b0c14ff7
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-08 22:34:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,10 @@ def monopoly_with_third_tier
     {
       :pieces => 4,
       :names => {},
-      :rent_in_dollars => {:one_piece_owned => 25}
+      :rent_in_dollars => {
+        :one_piece_owned => 25,
+        :two_piece_owned => 25
+      }
     }
   }
 end
